2704bbd7b38575cfbb9dfbc7ff5501065702fffb
Ekata Nagrale <ekatanag17@gmail.com>
2023-05-23 22:03:26 +0530

    First Commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+var items = document.getElementsByClassName('list-group-item');
+
+
+items[3].style.background = 'green';
+
+
+
+for(var i=0;i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
+
+
+
